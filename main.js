2f6cd1f4da65f0c5411dfa27c83567dbad47a3db
Champmannn <mrkjhnndy@gmail.com>
2026-06-01 19:39:14 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+// Form Validation
+const forms = document.querySelectorAll('.needs-validation');
 
+Array.from(forms).forEach((form) => {
+    form.addEventListener('submit', (event) => {
+        if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+        } else {
+            event.preventDefault();
+            alert('Form submitted successfully!');
+            form.reset();
+            form.classList.remove('was-validated');
+            bootstrap.Modal.getInstance(document.getElementById('contactModal')).hide();
+        }
+        form.classList.add('was-validated');
+    });
+});
